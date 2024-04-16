7b86fcb5f2d93cbd6c1543b5242b4f01f3446ed8
Luka Majstorovic <lukavumajstorovic@gmail.com>
2024-04-17 00:04:39 +0200

    First page in progress

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerBtn = document.querySelector('.hamburger');
-    const navBar = document.querySelector('.navigation-bar');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerBtn = document.querySelector(".hamburger");
+  const navBar = document.querySelector(".navigation-bar");
 
-    hamburgerBtn.addEventListener('click', function() {
-        navBar.classList.toggle('active');
-    });
+  hamburgerBtn.addEventListener("click", function () {
+    navBar.classList.toggle("active"); // Toggle the 'active' class on the navigation bar
+  });
 });
